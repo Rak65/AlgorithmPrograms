7ec4da32083455c1d6e71cc012329cac61e6ef87
Rak65 <rakeshp6505@gmail.com>
2023-05-21 17:00:14 +0530

    find the prime numbers that are Anagram and Palindrome

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -29,13 +29,51 @@ namespace AlgorithmProgram
             return true;
         }
 
+        // Function to check if a number is an anagram
+        public static bool IsAnagram(int number1, int number2)
+        {
+            string str1 = number1.ToString();
+            string str2 = number2.ToString();
+
+            // Convert the numbers to strings and sort the characters
+            char[] charArray1 = str1.ToCharArray();
+            char[] charArray2 = str2.ToCharArray();
+
+            Array.Sort(charArray1);
+            Array.Sort(charArray2);
+
+            // Check if the sorted strings are equal
+            string sortedStr1 = new string(charArray1);
+            string sortedStr2 = new string(charArray2);
+
+            return sortedStr1.Equals(sortedStr2);
+        }
+
+        // Function to check if a number is a palindrome
+        public static bool IsPalindrome(int number)
+        {
+            string str = number.ToString();
+            int length = str.Length;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (str[i] != str[length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Prime numbers in the range of 0 to 1000:");
+
+            Console.WriteLine("Prime numbers in the range of 0 to 1000 that are anagrams and palindromes:");
 
             for (int i = 0; i <= 1000; i++)
             {
-                if (IsPrime(i))
+                if (IsPrime(i) && IsAnagram(i, 0) && IsPalindrome(i))
                 {
                     Console.Write(i + " ");
                 }
@@ -43,7 +81,6 @@ namespace AlgorithmProgram
 
             Console.WriteLine();
             Console .ReadKey();
-
         }
     }
 }
