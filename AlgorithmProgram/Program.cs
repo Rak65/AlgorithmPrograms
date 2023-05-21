8e967e4594e331a96bdfa6124ce3c00ec0ec8867
Rak65 <rakeshp6505@gmail.com>
2023-05-21 16:43:41 +0530

    Take a range of 0 - 1000 Numbers and find the Prime numbers in that range.

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -9,48 +9,41 @@ namespace AlgorithmProgram
 {
     public class Program
     {
-        // Function to check if two strings are anagrams
-        public static bool IsAnagram(string str1, string str2)
+        // Function to check if a number is prime
+        public static bool IsPrime(int number)
         {
-            // Convert the strings to character arrays
-            char[] charArray1 = str1.ToCharArray();
-            char[] charArray2 = str2.ToCharArray();
+            if (number <= 1)
+            {
+                return false;
+            }
 
-            // Sort the character arrays
-            Array.Sort(charArray1);
-            Array.Sort(charArray2);
+            // Check divisibility from 2 to square root of the number
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
 
-            // Convert the sorted character arrays back to strings
-            string sortedStr1 = new string(charArray1);
-            string sortedStr2 = new string(charArray2);
-
-            // Check if the sorted strings are equal
-            return sortedStr1.Equals(sortedStr2);
+            return true;
         }
 
         public static void Main(string[] args)
         {
+            Console.WriteLine("Prime numbers in the range of 0 to 1000:");
 
-            // Prompt the user to enter two strings
-            Console.Write("Enter the first string: ");
-            string str1 = Console.ReadLine();
-
-            Console.Write("Enter the second string: ");
-            string str2 = Console.ReadLine();
-
-            // Check if the strings are anagrams
-            bool isAnagram = IsAnagram(str1, str2);
-
-            // Print the result
-            if (isAnagram)
+            for (int i = 0; i <= 1000; i++)
             {
-                Console.WriteLine("The two strings are anagrams.");
+                if (IsPrime(i))
+                {
+                    Console.Write(i + " ");
+                }
             }
-            else
-            {
-                Console.WriteLine("The two strings are not anagrams.");
-            }
+
+            Console.WriteLine();
             Console .ReadKey();
+
         }
     }
 }
