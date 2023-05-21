472e1420cb731632a2a59c6cd8ad60ec5ea16035
Rak65 <rakeshp6505@gmail.com>
2023-05-21 17:12:17 +0530

    Guess the answer

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -9,78 +9,45 @@ namespace AlgorithmProgram
 {
     public class Program
     {
-        // Function to check if a number is prime
-        public static bool IsPrime(int number)
+        // Recursive function to guess the number
+        public static int GuessNumberRecursive(int low, int high)
         {
-            if (number <= 1)
+            if (low == high)
             {
-                return false;
+                return low;
             }
 
-            // Check divisibility from 2 to square root of the number
-            for (int i = 2; i * i <= number; i++)
+            int mid = (low + high) / 2;
+
+            Console.Write("Is the number between {0} and {1}? (y/n): ", low, mid);
+            char response = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            if (response == 'y' || response == 'Y')
             {
-                if (number % i == 0)
-                {
-                    return false;
-                }
+                return GuessNumberRecursive(low, mid);
             }
-
-            return true;
-        }
-
-        // Function to check if a number is an anagram
-        public static bool IsAnagram(int number1, int number2)
-        {
-            string str1 = number1.ToString();
-            string str2 = number2.ToString();
-
-            // Convert the numbers to strings and sort the characters
-            char[] charArray1 = str1.ToCharArray();
-            char[] charArray2 = str2.ToCharArray();
-
-            Array.Sort(charArray1);
-            Array.Sort(charArray2);
-
-            // Check if the sorted strings are equal
-            string sortedStr1 = new string(charArray1);
-            string sortedStr2 = new string(charArray2);
-
-            return sortedStr1.Equals(sortedStr2);
-        }
-
-        // Function to check if a number is a palindrome
-        public static bool IsPalindrome(int number)
-        {
-            string str = number.ToString();
-            int length = str.Length;
-
-            for (int i = 0; i < length / 2; i++)
+            else
             {
-                if (str[i] != str[length - i - 1])
-                {
-                    return false;
-                }
+                return GuessNumberRecursive(mid + 1, high);
             }
-
-            return true;
         }
 
         public static void Main(string[] args)
         {
 
-            Console.WriteLine("Prime numbers in the range of 0 to 1000 that are anagrams and palindromes:");
+            Console.Write("Enter the value of N (where N = 2^n): ");
+            int n = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i <= 1000; i++)
-            {
-                if (IsPrime(i) && IsAnagram(i, 0) && IsPalindrome(i))
-                {
-                    Console.Write(i + " ");
-                }
-            }
+            int low = 0;
+            int high = (int)Math.Pow(2, n) - 1;
 
-            Console.WriteLine();
-            Console .ReadKey();
+            Console.WriteLine("Think of a number between 0 and {0} (inclusive).", high);
+
+            int guessedNumber = GuessNumberRecursive(low, high);
+
+            Console.WriteLine("The number you were thinking of is: " + guessedNumber);
+            Console.ReadKey();
         }
     }
 }
